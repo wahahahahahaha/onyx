2d54f169aa61a0f0a6075c1f4ac6fdcd145bc5dc
Joel N <cboar@users.noreply.github.com>
2015-10-17 03:56:12 -0700

    Added WIP Updater.

---
+++ updater/src/Loader.scala
@@ -0,0 +1,122 @@
+import collection.mutable.MutableList
+import collection.JavaConversions._
+import util.control.Exception
+import java.util.jar.JarFile
+import jdk.internal.org.objectweb.asm.ClassReader
+import jdk.internal.org.objectweb.asm.tree._
+
+class Loader(val rev: Int){
+
+	val classes = loadClasses()
+	val methods = loadMethods()
+	val fields = loadFields()
+
+	private def findMethod(full: String) = {
+		val Array(owner, name, desc) = full.split(" ")
+		classes(owner).methods.find(m => m.name == name && m.desc == desc)
+	}
+
+	private def findSupers(clname: String) = {
+		val result = collection.mutable.Set[ClassNode]()
+		Exception.ignoring(classOf[NoSuchElementException]) {
+			var target = classes(clname)
+			while(!target.name.contains("java")){
+				result += target
+				target = classes(target.superName)
+			}
+		}
+		result.toSet
+	}
+
+	private def findSubs(clname: String): Set[ClassNode] = {
+		try {
+			findSubs(classes(clname))
+		} catch {
+			case e: NoSuchElementException => Set()
+		}
+	}
+
+	private def findSubs(clazz: ClassNode): Set[ClassNode] = {
+		val result = collection.mutable.Set[ClassNode]( clazz )
+		for(c <- classes.values; if c.superName == clazz.name)
+			result ++= findSubs( c )
+		result.toSet
+	}
+
+	private def findRelated(clname: String) = {
+		findSubs(clname) ++ findSupers(clname)
+	}
+
+	private def findMethods(full: String) = {
+		val Array(owner, name, desc) = full.split(" ")
+		val related = findRelated(owner)
+		val result = collection.mutable.Map[String, MethodNode]()
+
+		for(c <- related){
+			val temp = c.name + " " + name + " " + desc
+			val method = findMethod(temp)
+			if(method != None) result += (temp -> method.get)
+		}
+		result.toMap
+	}
+
+	private def addMethods(methods: collection.mutable.Map[String, MethodNode], full: String){
+		val found = findMethods(full)
+		methods ++= found
+
+		for(m <- found; i <- m._2.instructions.iterator; if isInvoke(i.getOpcode)){
+			val insn = i.asInstanceOf[MethodInsnNode]
+			val desc = insn.owner + " " + insn.name + " " + insn.desc
+			if(!methods.contains(desc))
+				addMethods(methods, desc)
+		}
+	}
+
+	private def loadFields() = {
+		val fields = collection.mutable.Map[String, FieldNode]()
+		for(m <- methods.values; i <- m.instructions.iterator; if isField(i.getOpcode)){
+			val insn = i.asInstanceOf[FieldInsnNode]
+			val supers = findSupers(insn.owner)
+			for(c <- supers){
+				Exception.ignoring(classOf[NoSuchElementException]){
+					val field = c.fields.find(_.name == insn.name).get
+					fields += (c.name + "." + insn.name -> field)
+				}
+			}
+		}
+		for(c <- classes.values; f <- c.fields.toList; if !fields.values.contains(f))
+			c.fields.remove(f)
+
+		log("Kept " + fields.size + " fields.")
+		fields.toMap
+	}
+
+	private def loadMethods() = {
+		val methods = collection.mutable.Map[String, MethodNode]()
+		for(c <- classes.values; m <- c.methods; if m.name.length > 2; if m.name != "<init>")
+			addMethods(methods, c.name + " " + m.name + " " + m.desc)
+
+		for(c <- classes.values; m <- c.methods.toList; if !methods.values.contains(m))
+			c.methods.remove(m)
+
+		log("Kept " + methods.size + " methods.")
+		methods.toMap
+	}
+
+	private def loadClasses() = {
+		var classes = collection.mutable.Map[String, ClassNode]()
+		val jar = new JarFile(rev + ".jar")
+		for(e <- jar.entries; if e.getName.endsWith(".class")){
+			val reader = new ClassReader(jar.getInputStream(e))
+			val node = new ClassNode()
+			reader.accept(node, ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES)
+			classes += (node.name -> node)
+		}
+		jar.close()
+		classes.toMap
+	}
+
+	def log(str: String) = println("(" + rev + ") " + str)
+	def isField(code: Int) = (code >= 0xb2 && code <= 0xb5)
+	def isInvoke(code: Int) = (code >= 0xb6 && code <= 0xba)
+}
---
+++ updater/src/Main.scala
@@ -0,0 +1,52 @@
+import collection.mutable._
+import collection.JavaConversions._
+import jdk.internal.org.objectweb.asm.tree._
+
+object Main extends App {
+	val cl94 = new Loader(94)
+	val cl90 = new Loader(90)
+
+	val node1 = cl94.classes("f")
+	val node2 = cl90
+
+	val results = MutableList[(String, Double)]()
+	for(c <- cl90.classes.values)
+		results += (c.name -> compare(c, node1, toFieldList))
+
+	results.sortWith(_._2 > _._2).foreach(i => println(i._1 + ": " + i._2))
+
+	def compare(one: ClassNode, two: ClassNode, func: (ClassNode) => List[String]) = {
+		val count = new DynamicMap
+		func(one).foreach(f => count.inc(f))
+		func(two).foreach(f => count.dec(f))
+
+		val total = if(count.incs > count.decs) count.incs else count.decs
+		val amt = count.map.values.foldLeft(0)(Math.abs(_) + Math.abs(_))
+		val percent = (total - amt * 0.5) / total
+		percent
+	}
+
+	def fixObjNames(str: String) = str.replaceAll("L[a-z][a-z]?;", "O")
+	def toMethodList(cl: ClassNode) = cl.methods.toList.map(m => m.access + fixObjNames(m.desc))
+	def toFieldList(cl: ClassNode) = cl.fields.toList.map(f => f.access + fixObjNames(f.desc))
+
+	def all(loader: Loader) = {
+		val result = Map[String, Map[String, Int]]()
+		for(m <- loader.methods; i <- m._2.instructions.iterator;
+		if i.getOpcode == 0xb4 || i.getOpcode == 0xb5){
+			val field = i.asInstanceOf[FieldInsnNode]
+			val name = field.owner + "." + field.name
+			if(loader.fields.contains(name)){
+
+				if(!result.contains(name))
+					result += (name -> Map[String, Int]())
+				if(!result(name).contains(m._1))
+					result(name) += (m._1 -> 0)
+
+				result(name)(m._1) += 1
+			}
+		}
+		result
+	}
+
+}
